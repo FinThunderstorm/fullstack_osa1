671758866efbb0cfb9f467605bf8ab008314cfdb
Tuomas Alanen <tuomas@paaryna.fi>
2019-06-16 19:24:10 +0300

    1.7
    unicafe step2

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -8,19 +8,29 @@ const Button = (props) => (
     </button>
 )
 
-const Statistics = props => (
+const Statistics = props => {
+    const all = props.goodValue + props.neutralValue + props.badValue
+    const average = (props.goodValue+(props.badValue*-1))/all
+    const positive = props.goodValue/all*100
+
+    return (
     <>
         <h1>statistics</h1>
         <div>good: {props.goodValue}</div>
         <div>neutral: {props.neutralValue}</div>
         <div>bad: {props.badValue}</div>
+        <div>all: {all}</div> 
+        <div>average: {average}</div>
+        <div>positive: {positive}%</div>
+
     </>
-)
+    )
+}
 
 const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0)
+    const [bad, setBad] = useState(0)      
 
     const setToGood = value => {
         setGood(value)
